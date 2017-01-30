50e343a3fcdb373b2fba8f63885379021bb51178
caltras <caltras@gmail.com>
2017-01-30 11:32:35 +0000

    Ajuste na configuração da fila

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,6 @@ exports.NODE_ENV    = process.env.NODE_ENV || 'development';
 exports.PORT        = process.env.PORT || 3000;
 exports.IP          = process.env.IP || "127.0.0.0";
 exports.MONGO_URL   = process.env.MONGO_URL;
-exports.CLOUDAMQP_URL   = process.env.MQ_URL;
+exports.CLOUDAMQP_URL   = process.env.CLOUDAMQP_URL;
 exports.QUEUE_PESSOA_UPDATE = process.env.QUEUE_PESSOA_UPDATE;
 exports.MAILGUN_API = process.env.MAILGUN_API;
